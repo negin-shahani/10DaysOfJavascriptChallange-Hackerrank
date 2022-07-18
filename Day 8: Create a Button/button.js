b0627511e747a5a0c3e50c797d1ec7049ead03e6
negin banay shahani <42892521+negin-shahani@users.noreply.github.com>
2022-07-18 12:43:37 +0430

    update button.js with a professional answer

--- Day 8: Create a Button/button.js
+++ Day 8: Create a Button/button.js
@@ -2,3 +2,14 @@ btn.onclick = function() {
     const count = Number(btn.innerHTML) + 1;    
     btn.innerText = count;
 }
+
+//another answer I found after submitting
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('btn');
+    
+    button.addEventListener('click', (e) => {
+          const count = Number(e.currentTarget.innerText) + 1;
+        
+          e.currentTarget.innerText = count;
+     });
+});
